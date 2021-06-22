b302550dded99e885541e0ddd2d686a0176610d4
JuanSaz <juan.snchez03@gmail.com>
2021-06-22 20:39:27 -0300

    Some changes were made // Check if user exists - Pending //

--- app/src/main/java/com/jdev/gamma/createActivity.kt
+++ app/src/main/java/com/jdev/gamma/createActivity.kt
@@ -1,14 +1,17 @@
 package com.jdev.gamma
 
 import android.content.Intent
+import android.nfc.Tag
 import android.os.Bundle
 import android.provider.ContactsContract.CommonDataKinds.Email
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_auth.*
 import kotlinx.android.synthetic.main.activity_auth.inputContraseña
 import kotlinx.android.synthetic.main.activity_auth.inputEmail
@@ -23,6 +26,7 @@ class createActivity : AppCompatActivity() {
         setContentView(R.layout.activity_create)
         setup()
         passData()
+        getDB()
     }
 
     private fun setup(){
@@ -32,7 +36,7 @@ class createActivity : AppCompatActivity() {
         )
 
         crearCuentaButton.setOnClickListener{
-            if (inputEmail.text.isNotEmpty() && inputContraseña.text.isNotEmpty()){
+            if (inputEmail.text.isNotEmpty() && inputContraseña.text.isNotEmpty()&& inputUsername.text.isNotEmpty()){
                 db.collection("users").document(inputEmail.text.toString()).set(
                     hashMapOf("email" to inputEmail.text.toString(),
                              "username" to inputUsername.text.toString(),
@@ -40,21 +44,34 @@ class createActivity : AppCompatActivity() {
                     )
                 )
                 FirebaseAuth.getInstance().createUserWithEmailAndPassword(inputEmail.text.toString(), inputContraseña.text.toString()) .addOnCompleteListener {
+                    if (it.isSuccessful){
                         showAuth(it.result?.user?.email ?: "", ProviderType.Email)
-                        val toast = Toast.makeText(applicationContext, "Usuario Creado", Toast.LENGTH_SHORT)
+                        val toast =Toast.makeText(applicationContext, "Usuario Creado", Toast.LENGTH_SHORT)
+                        toast.show()
+                    } else {
+                        val toast =Toast.makeText(applicationContext, "Datos erroneos", Toast.LENGTH_SHORT)
                         toast.show()
                     }
-
-             }
-            else{
-                val toast = Toast.makeText(applicationContext, "Datos vacios", Toast.LENGTH_SHORT)
-                toast.show()
-                //showError()
+                }
             }
           }
        }
 
-
+    private fun getDB(){
+        check_Button.setOnClickListener{
+                    db.collection("users").document(inputEmail.text.toString()).get().addOnSuccessListener { document ->
+                    if(document.exists()){
+                        val username: String? =document.getString("username")
+                        val toast =Toast.makeText(applicationContext, "Usuario existente", Toast.LENGTH_SHORT)                                  // Sintax para imprimir variables y texto
+                        Log.d("Doc Data: ","$username")
+                    }
+                    else{
+                        Log.d("Doc Data: ","Null")
+                        val toast =Toast.makeText(applicationContext, "Usuario inexistente", Toast.LENGTH_SHORT)
+                    }
+                }
+            }
+        }
 
     private fun showError(){
         val builder = AlertDialog.Builder(this)
@@ -62,8 +79,6 @@ class createActivity : AppCompatActivity() {
         builder.setPositiveButton("Aceptar", null)
         val dialog: AlertDialog = builder.create()
         dialog.show()
-
-
     }
 
     private fun showAuth(email: String, provider: ProviderType){
